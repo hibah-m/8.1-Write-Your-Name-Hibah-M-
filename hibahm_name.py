ed1a1cf9a74851765b18658cd4f2d5f6241b68cd
Your Name <hibahmohammad1228@gmail.com>
2026-04-02 16:34:58 -0500

    We have practiced using turtle, just started letters

---
+++ hibahm_name.py
@@ -0,0 +1,58 @@
+from turtle import *
+
+
+#screen = Screen()
+#screen.title("Write Your Name! ")
+#screen.bgcolor("Purple")
+#screen.setup(width = 1000, height = 400)
+
+
+#turt = Turtle()
+#turt.shape("turtle")
+
+#for j in range(4):
+#    for i in range(5):
+#        turt.forward(10)
+#        turt.penup()
+#        turt.forward(10)
+#        turt.pendown()
+#    turt.left(90)
+
+
+#while True:
+#    angle = int(input("What heading would you like Turt to face?"))
+#    turt.setheading(angle)
+
+#burt = Turtle()
+#burt.pu()
+#burt.color("red")
+#burt.goto(100,-100)
+
+#turt.pu()
+#turt.goto(-100,-100)
+#turt.pd()
+#turt.sety(100)
+#turt.setx(100)
+
+#turt.goto(burt.position())
+
+#for i in range(180):
+#    turt.forward(5)
+#    turt.right(1)
+
+#turt.circle(-50,180)
+
+
+screen = Screen()
+screen.title("Write Your Name! ")
+screen.bgcolor("White")
+screen.setup(width = 1000, height = 500)
+
+
+
+
+
+
+
+
+screen.exitonclick()
